acc31d1c9585bb026d9c8204861cf639e753efb9
ElliotBruhl <166664797+ElliotBruhl@users.noreply.github.com>
2024-10-01 14:35:52 +0000

    in progress

--- .ccls-cache/@home@runner@Functions-Programming-Project/main.cpp
+++ .ccls-cache/@home@runner@Functions-Programming-Project/main.cpp
@@ -1,3 +1,31 @@
 #include <iostream>
 
-int main() { std::cout << "Hello World!\n"; }
+void displayResults(double& rate1, double& rate2) {
+  
+}
+void calcRates(double& price1, double& price2, double& price3, double& rate1, double& rate2) { //take in prices and put resulting rates in rate1 and rate2
+  
+}
+void getPrices(double& price1, double& price2, double& price3) { //pass by reference (rather than passing a copy) to change values in main
+  std::cout << "Enter the price of item 1: ";
+  std::cin >> price1;
+  std::cout << "Enter the price of item 2: ";
+  std::cin >> price2;
+  std::cout << "Enter the price of item 3: ";
+  std::cin >> price3;
+
+  calcRates(price1, price2, price3);
+}
+
+int main() {
+  
+  double price1, price2, price3;
+  getPrices(price1, price2, price3);
+  
+  double rate1, rate2;
+  calcRates(rate1, rate2);
+  
+  displayResults(rate1, rate2);
+  
+  return 0;
+}
